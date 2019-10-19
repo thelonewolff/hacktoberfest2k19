246180146b97f743f750229b6c5023f2397d900a
thelonewolff <56663315+thelonewolff@users.noreply.github.com>
2019-10-20 00:46:37 +0530

    Update primefactorization.py

--- Prime Factors/primefactorization.py
+++ Prime Factors/primefactorization.py
@@ -1,15 +1,15 @@
 n=int(input("Enter an integer:"))
 print("Factors are:")
-i=0
-for(i=0;i<n;i++)
-while(i<=n):
-    k=0
-    if(n%i==0):
-        j=1
-        while(j<=i):
-            if(i%j==1):
-                k=k+1
-            j=j+1
-        if(k==2):
-            print(i)
-    i=i+1
+
+def fn(a):
+    c=0
+    for i in range(1,a+1):
+        if a % i ==0:
+            c=c+1
+    if c == 2:
+            print(a)
+            
+for i in range(1,n+1):
+    
+    if n % i == 0 :
+        fn(i)
